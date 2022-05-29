44a95bf95720dd1a543b5a546a8595e03b9061ff
trung2906 <chimsedigios@gmail.com>
2022-05-29 09:14:02 +0700

    up homework

---
+++ L2DataStructure/cau2.py
@@ -0,0 +1,12 @@
+A=110
+B=262
+rs1=A+B
+print(f"Result of {A} + {B} equal {rs1}")
+rs2=A-B
+print(f"Result of {A} - {B} equal {rs2}")
+rs3=A/B
+print(f"Result of {A} / {B} equal {rs3}")
+rs4=A//B
+print(f"Result of {A} // {B} equal {rs4}")
+rs5=A%B
+print(f"Result of {A} % {B} equal {rs5}")
---
+++ L2DataStructure/cau3.py
@@ -0,0 +1,9 @@
+stringmain="Python programming is the best course to learn"
+print(len(stringmain))
+print(stringmain[:4])
+print(stringmain[-10:])
+print(stringmain.replace("Python","DUT"))
+print(stringmain.find("best"))
+print(stringmain.isupper())
+print(stringmain.split(' '))
+
